0c9876854a05eb0ec41667e6d05096e7955c86e0
Premkumar Savale <premsavle112@gmail.com>
2025-12-25 15:43:56 +0530

    Added Code

--- src/blocks/MediBlock/Component.tsx
+++ src/blocks/MediBlock/Component.tsx
@@ -1,5 +1,7 @@
 
+import RichText from "@/components/RichText";
 import Image from "next/image";
+import { type DefaultTypedEditorState } from '@payloadcms/richtext-lexical';
 
 interface MediBlockProps {
 
@@ -8,29 +10,51 @@ interface MediBlockProps {
         url: string;
         alt: string;
 
-    }
+    },
+    richText: DefaultTypedEditorState,
+    Paragraph?: {
+        Point: string;
+        id?: string;
+    }[],
+
 }
 
 
 
-export const MediBlock: React.FC<MediBlockProps> = ({ image }) => {
+export const MediBlock: React.FC<MediBlockProps> = ({ image, richText, Paragraph }) => {
     // console.log(Image);
 
     return (
 
         <>
-            {
-                image && (
 
-                    <Image
-                        src={image.url}
-                        alt={image.alt}
-                        height={400}
-                        width={500}
-                        className="mx-auto w-[80%]"
-                    />
-                )
-            }
+
+            <div className="relative video-wrapper -mt-[110px]">
+
+                {
+                    image && (
+
+                        <Image
+                            src={image.url}
+                            alt={image.alt}
+                            height={400}
+                            width={800}
+                            className="mx-auto w-[80%]"
+                        />
+                    )
+                }
+                <div className="absolute inset-0 flex items-center justify-center text-white p-4">
+                    {richText && <RichText className="mb-0" data={richText} enableGutter={false} />}
+                </div>
+
+                <div className="absolute inset-0 flex items-center justify-center text-white p-4 mt-[230px]">
+                    <ul className="list-disc pl-5 space-y-4 text-white" >
+                        {Paragraph?.map((item, index) => (
+                            <li key={index} style={{ fontSize: '20px' }}>   <b> {item.Point}    </b>  </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
 
         </>
     )
